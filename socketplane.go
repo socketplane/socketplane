1a6a5b1236eb2a52eb100b0706659f3d776ef01d
Madhu Venugopal <madhu@socketplane.io>
2014-12-07 06:27:40 -0800

    Updated the Socketplane application to use the new Bonjour daemon service
    Signed-off-by: Madhu Venugopal <madhu@socketplane.io>

--- socketplane.go
+++ socketplane.go
@@ -1,8 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/socketplane/bonjour"
+)
+
+const DOCKER_CLUSTER_SERVICE = "_docker._cluster"
+const DOCKER_CLUSTER_SERVICE_PORT = 9999 //TODO : fix this
+const DOCKER_CLUSTER_DOMAIN = "local"
 
 func main() {
 	go Bonjour("eth1")
 	fmt.Println("HELLO SOCKETPLANE")
+}
+
+func Bonjour(intfName string) {
+	b := bonjour.Bonjour{
+		ServiceName:     DOCKER_CLUSTER_SERVICE,
+		ServiceDomain:   DOCKER_CLUSTER_DOMAIN,
+		ServicePort:     DOCKER_CLUSTER_SERVICE_PORT,
+		InterfaceName:   intfName,
+		OnMemberHello:   newMember,
+		OnMemberGoodBye: removeMember,
+	}
+	b.Start()
+}
+
+func newMember(addr net.IP) {
+	log.Println("New Member Added : ", addr)
+}
+func removeMember(addr net.IP) {
+	log.Println("Member Left : ", addr)
 }
