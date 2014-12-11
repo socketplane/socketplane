37549c7d5f77d1f03f1404961867d5416aad8fa7
Madhu Venugopal <madhu@socketplane.io>
2014-12-11 05:38:42 -0800

    Implemented a simple and slightly better version of IPAM using byte arrays for bit mgmt
    Signed-off-by: Madhu Venugopal <madhu@socketplane.io>

--- ipam/ipam.go
+++ ipam/ipam.go
@@ -3,16 +3,14 @@ package ipam
 import (
 	"errors"
 	"log"
+	"math"
 	"net"
 
 	"github.com/socketplane/socketplane/Godeps/_workspace/src/github.com/socketplane/ecc"
 )
 
-// Naive, Quick and Dirty IPv4 IPAM solution using Consul Distributed KV store
-// Key = subnet, Value = next available ip-address numerically
-// Yes. Naive.
-
-// TODO : Implement a more robust default IPAM
+// Simple IPv4 IPAM solution using Consul Distributed KV store
+// Key = subnet, Value = Bit Array of available ip-addresses in a given subnet
 
 const dataDir = "/tmp/socketplane"
 const dataStore = "ipam"
@@ -21,6 +19,99 @@ func Init(bindInterface string, bootstrap bool) error {
 	return ecc.Start(true, bootstrap, bindInterface, dataDir)
 }
 
+func Request(subnet net.IPNet) net.IP {
+	bits := bitCount(subnet)
+	bc := int(bits / 8)
+	partial := int(math.Mod(bits, float64(8)))
+
+	if partial != 0 {
+		bc += 1
+	}
+	addrArray, _, ok := ecc.Get(dataStore, subnet.String())
+	currVal := make([]byte, len(addrArray))
+	copy(currVal, addrArray)
+	if !ok {
+		addrArray = make([]byte, bc)
+	}
+	pos := testAndSetBit(addrArray)
+	eccerr := ecc.Put(dataStore, subnet.String(), addrArray, currVal)
+	if eccerr == ecc.OUTDATED {
+		return Request(subnet)
+	}
+	return getIP(subnet, pos)
+}
+
+func Release(address net.IPNet) bool {
+	return true
+}
+
+// Given Subnet of interest and free bit position, this method returns the corresponding ip address
+// This method is functional and tested. Refer to ipam_test.go But can be improved
+
+func getIP(subnet net.IPNet, pos uint) net.IP {
+	retAddr := make([]byte, len(subnet.IP))
+	copy(retAddr, subnet.IP)
+
+	mask, _ := subnet.Mask.Size()
+	var tb, byteCount, bitCount int
+	if subnet.IP.To4() != nil {
+		tb = 4
+		byteCount = (32 - mask) / 8
+		bitCount = (32 - mask) % 8
+	} else {
+		tb = 16
+		byteCount = (128 - mask) / 8
+		bitCount = (128 - mask) % 8
+	}
+
+	for i := 0; i <= byteCount; i++ {
+		maskLen := 0xFF
+		if i == byteCount {
+			if bitCount != 0 {
+				maskLen = int(math.Pow(2, float64(bitCount))) - 1
+			} else {
+				maskLen = 0
+			}
+		}
+		masked := pos & uint((0xFF&maskLen)<<uint(8*i))
+		retAddr[tb-i-1] |= byte(masked >> uint(8*i))
+	}
+	return net.IP(retAddr)
+}
+
+func bitCount(addr net.IPNet) float64 {
+	mask, _ := addr.Mask.Size()
+	if addr.IP.To4() != nil {
+		return math.Pow(2, float64(32-mask))
+	} else {
+		return math.Pow(2, float64(128-mask))
+	}
+}
+
+func setBit(a []byte, k uint) {
+	a[k/8] |= 1 << (k % 8)
+}
+
+func clearBit(a []byte, k uint) {
+	a[k/8] &= ^(1 << (k % 8))
+}
+
+func testBit(a []byte, k uint) bool {
+	return ((a[k/8] & (1 << (k % 8))) != 0)
+}
+
+func testAndSetBit(a []byte) uint {
+	var i uint
+	for i = uint(0); i < uint(len(a)*8); i++ {
+		if !testBit(a, i) {
+			setBit(a, i)
+			return i + 1
+		}
+	}
+	return i
+}
+
+// Lame implementation. Deprecate favoring Request function
 func GetAnAddress(subnet string) (string, error) {
 	addrArray, _, ok := ecc.Get(dataStore, subnet)
 	currVal := make([]byte, len(addrArray))
@@ -39,8 +130,10 @@ func GetAnAddress(subnet string) (string, error) {
 	addrArray[3] += 1
 
 	eccerr := ecc.Put(dataStore, subnet, addrArray, currVal)
-	if eccerr != ecc.OK {
+	if eccerr == ecc.OUTDATED {
 		return GetAnAddress(subnet)
+	} else if eccerr != ecc.OK {
+		return "", errors.New("Unable to get ip-address ")
 	}
 	addrArray[3] -= 1
 	return net.IP(addrArray).String(), nil
--- ipam/ipam_test.go
+++ ipam/ipam_test.go
@@ -1,7 +1,9 @@
 package ipam
 
 import (
+	"fmt"
 	"log"
+	// "log"
 	"net"
 	"testing"
 
@@ -15,9 +17,35 @@ func TestInit(t *testing.T) {
 	}
 }
 
-func TestGetIp(t *testing.T) {
+func TestGetIpFullMask(t *testing.T) {
+	count := 2500
+	for i := 1; i < count; i++ {
+		_, ipNet, _ := net.ParseCIDR("192.168.0.0/16")
+		address := Request(*ipNet)
+		address = address.To4()
+		fmt.Println(address.String())
+		if i%256 != int(address[3]) || i/256 != int(address[2]) {
+			t.Error(address.String())
+		}
+	}
+}
+
+func TestGetIpPartialMask(t *testing.T) {
+	count := 1000
+	for i := 1; i < count; i++ {
+		_, ipNet, _ := net.ParseCIDR("192.169.32.0/20")
+		address := Request(*ipNet)
+		address = address.To4()
+		fmt.Println(address.String())
+		if i%256 != int(address[3]) || 32+i/256 != int(address[2]) {
+			t.Error(address.String())
+		}
+	}
+}
+
+func TestGetIpDeprecated(t *testing.T) {
 	for i := 1; i < 250; i++ {
-		addressStr, err := GetAnAddress("192.168.1.0/24")
+		addressStr, err := GetAnAddress("192.167.1.0/24")
 		if err != nil {
 			log.Println(err)
 			t.Fatal(err)
@@ -30,6 +58,8 @@ func TestGetIp(t *testing.T) {
 }
 
 func TestCleanup(t *testing.T) {
-	ecc.Delete(dataStore, "192.168.1.0/24")
+	ecc.Delete(dataStore, "192.167.1.0/24")
+	ecc.Delete(dataStore, "192.168.0.0/16")
+	ecc.Delete(dataStore, "192.169.32.0/20")
 	Leave()
 }
