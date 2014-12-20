6ddc86d06786d81b911e2d8195865a6feefa5ffe
Dave Tucker <dave@socketplane.io>
2014-12-20 14:49:48 +0000

    Tidy up
    - Fix #8
    - More robust OS checks now run with script, not as a func
    - Move scripts to ./scripts to avoid clash with socketplane binary
    - Add better utility functions for logging
    - Refactor utilites to functions.sh so they can be re-used
    - Reduce os-specific clutter
    - Add some more logging statements
    - Fixup some additional synatx errors on bash to sh conversion
    
    Signed-off-by: Dave Tucker <dave@socketplane.io>

---
+++ scripts/functions.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# Utility function to test if a command exists
+command_exists() {
+    hash $@ 2>/dev/null
+}
+
+# Colorized Command Output
+log_info() {
+    printf "\033[0;36m$@\033[0m\n"
+}
+
+log_notice() {
+    printf "\033[0;32m$@\033[0m\n"
+}
+
+log_warn() {
+    printf "\033[0;33m$@\033[0m\n"
+}
+
+log_error() {
+    printf "\033[0;35m$@\033[0m\n"
+}
+
+log_fatal() {
+    printf "\033[0;31m$@\033[0m\n"
+}
+
+log_debug() {
+    printf "\033[0;37m$@\033[0m\n"
+}
+
+log_step() {
+    log_notice "-----> $@"
+}
+
+indent() {
+    sed -u "s/^/           /"
+}
