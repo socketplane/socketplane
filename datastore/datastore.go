1fdb68aaeaa53ff89622ce14f279d66144946148
Brent Salisbury <brent@socketplane.io>
2014-12-26 16:05:31 -0500

    Branch for adding nodes to the cluster
    patch2- typo and changed cli API
    changed endpoints/cluster
    -usage should be:
    socketplane.sh agent add-endpoint
    socketplane.sh agent del-endpoint
    
    Havent tested the API calls yet
    Associated with request:
    https://github.com/socketplane/socketplane/issues/53
    
    Signed-off-by: Brent Salisbury <brent@socketplane.io>

--- datastore/datastore.go
+++ datastore/datastore.go
@@ -1,6 +1,12 @@
 package datastore
 
-import "github.com/socketplane/socketplane/Godeps/_workspace/src/github.com/socketplane/ecc"
+import (
+	"errors"
+	log "github.com/socketplane/socketplane/Godeps/_workspace/src/github.com/Sirupsen/logrus"
+	"github.com/socketplane/socketplane/Godeps/_workspace/src/github.com/socketplane/ecc"
+	"github.com/socketplane/socketplane/ovs"
+	"net"
+)
 
 const dataDir = "/tmp/socketplane"
 
@@ -14,4 +20,27 @@ func Join(address string) error {
 
 func Leave() error {
 	return ecc.Leave()
+}
+
+type EndPoint struct{}
+
+func (n EndPoint) NewEndpoint(addr net.IP) error {
+	log.Info("New Member Added : ", addr)
+	Join(addr.String())
+	err := ovs.AddPeer(addr.String())
+	if err != nil {
+		return errors.New("Failed to add new peer")
+	}
+	log.Info("Added Member : ", addr)
+	return nil
+}
+
+func (n EndPoint) RemoveEndpoint(addr net.IP) error {
+	log.Info("Member Left : ", addr)
+	err := ovs.DeletePeer(addr.String())
+	if err != nil {
+		return errors.New("Failed to add new peer")
+	}
+	log.Info("Deleted Member : ", addr)
+	return nil
 }
